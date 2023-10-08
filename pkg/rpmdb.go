1549ae89f31e8eaa3802d61389853922ead543e5
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2023-10-08 13:48:52 +0000

    init

--- pkg/rpmdb.go
+++ pkg/rpmdb.go
@@ -1,10 +1,10 @@
 package rpmdb
 
 import (
-	"github.com/knqyf263/go-rpmdb/pkg/bdb"
-	dbi "github.com/knqyf263/go-rpmdb/pkg/db"
-	"github.com/knqyf263/go-rpmdb/pkg/ndb"
-	"github.com/knqyf263/go-rpmdb/pkg/sqlite3"
+	"github.com/khulnasoft-lab/go-rpmdb/pkg/bdb"
+	dbi "github.com/khulnasoft-lab/go-rpmdb/pkg/db"
+	"github.com/khulnasoft-lab/go-rpmdb/pkg/ndb"
+	"github.com/khulnasoft-lab/go-rpmdb/pkg/sqlite3"
 	"golang.org/x/xerrors"
 )
 
--- pkg/sqlite3/sqlite3.go
+++ pkg/sqlite3/sqlite3.go
@@ -6,7 +6,7 @@ import (
 	"encoding/binary"
 	"os"
 
-	dbi "github.com/knqyf263/go-rpmdb/pkg/db"
+	dbi "github.com/khulnasoft-lab/go-rpmdb/pkg/db"
 	"golang.org/x/xerrors"
 )
 
